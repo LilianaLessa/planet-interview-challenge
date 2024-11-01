815a65e01f7972b294bc17046043b13eccd908b7
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2024-11-01 16:38:10 +0000

    Modernization - CartItem
    - add type hints
    - fix return type on is_available
    - expiration time generation moved to a separated method
    - fix readability and robustness of getState
    - fix indentation

--- src/Shop/CartItem.php
+++ src/Shop/CartItem.php
@@ -6,58 +6,65 @@ use Planet\InterviewChallenge\App;
 
 class CartItem
 {
-		const MODE_NO_LIMIT = 0;
+    const MODE_NO_LIMIT = 0;
 
-		const MODE_HOUR = 1;
+    const MODE_HOUR = 1;
 
-		const MODE_MINUTE = 10;
+    const MODE_MINUTE = 10;
 
-		const MODE_SECONDS = 1000;
+    const MODE_SECONDS = 1000;
 
-		private int $expires;
+    private int $expires;
 
-		private int $price;
+    private int $price;
 
-		public function __construct(int $price, $mode, $modifier = null)
-		{
-		    switch ($mode) {
-		    case self::MODE_NO_LIMIT:
-		        $this->expires = -2;
-		        break;
-		    case self::MODE_HOUR:
-		        $this->expires = strtotime('+1 hour');
-                break;
-		    case self::MODE_MINUTE:
-		        $this->expires = strtotime('+' . $modifier . ' minutes');
-		        break;
-		    case self::MODE_SECONDS:
-		        $this->expires = strtotime('+' . $modifier . ' seconds');
-		        break;
-		    }
-		    $this->price = $price;
-		}
+    public function __construct(int $price, int $mode, ?int $modifier = null)
+    {
+        $this->expires = $this->generateExpiration($mode, $modifier);
+        $this->price = $price;
+    }
 
-		public function is_available(): ?bool
-		{
-            return $this->expires <= time();
-		}
+    public function is_available(): bool
+    {
+        return $this->expires <= time();
+    }
 
-       /**
-        * Returns the state representation of the object.
-        *
-        * @param int $format Constant from the class CartItem
-        * @return string|object State representation of the class.
-        */
-		public function getState(): string
-		{
-		    return '{"price":' . $this->price . ',"expires":'.$this->expires.'}';
-		}
+   /**
+    * Returns the state representation of the object.
+    *
+    * @param int $format Constant from the class CartItem
+    * @return string|object State representation of the class.
+    */
+    public function getState(): string
+    {
+        return json_encode(
+            [
+                "price" => $this->price,
+                "expires" => $this->expires,
+            ],
+            JSON_FORCE_OBJECT
+        );
+    }
 
-		public function display(): string
-		{
-		    App::smarty()->assign('price', $this->price);
-		    App::smarty()->assign('expires', $this->expires);
+    public function display(): string
+    {
+        App::smarty()->assign('price', $this->price);
+        App::smarty()->assign('expires', $this->expires);
 
-		    return App::smarty()->fetch('shop/CartItem.tpl');
-		}
+        return App::smarty()->fetch('shop/CartItem.tpl');
+    }
+
+    private function generateExpiration(int $mode, ?int $modifier = null): int
+    {
+        switch ($mode) {
+            case self::MODE_NO_LIMIT:
+                return -2;
+            case self::MODE_HOUR:
+                return strtotime('+1 hour');
+            case self::MODE_MINUTE:
+                return strtotime('+' . $modifier . ' minutes');
+            case self::MODE_SECONDS:
+                return strtotime('+' . $modifier . ' seconds');
+        }
+    }
 }
